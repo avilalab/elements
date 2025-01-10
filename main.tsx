930b92553c06edaa60762e37121aafbb34289e80
Fernando Avila <fernandoavilajunior@gmail.com>
2025-01-09 21:24:16 -0300

    Some path ajustments

---
+++ main.tsx
@@ -0,0 +1,26 @@
+export { Alert } from './src/components/alert/Alert';
+
+export { Button } from "./src/components/buttons/Button/Button";
+
+export { Card } from "./src/components/card/Card/Card";
+
+export { Footer } from "./src/components/common/Footer/Footer";
+export { Header } from "./src/components/common/Header/Header";
+
+export { ForgotPasswordForm } from './src/components/forms/ForgotPasswordForm/ForgotPasswordForm';
+export { LoginForm } from './src/components/forms/LoginForm/LoginForm';
+export { ResetCodeForm } from './src/components/forms/ResetCodeForm/ResetCodeForm';
+export { ResetPasswordForm } from './src/components/forms/ResetPasswordForm/ResetPasswordForm';
+export { SignUpForm } from './src/components/forms/SignUpForm/SignUpForm';
+
+export { BasicInput } from './src/components/inputs/BasicInput/BasicInput';
+export { Checkbox } from './src/components/inputs/Checkbox/Checkbox';
+export { Email } from './src/components/inputs/Email/Email';
+export { Text } from './src/components/inputs/Text/Text';
+export { TextArea } from './src/components/inputs/TextArea/TextArea';
+
+export { Loading } from './src/components/Loading/Loading';
+
+export { PostContentWidget } from './src/components/widgets/PostContentWidget/PostContentWidget';
+export { PostMetaWidget } from './src/components/widgets/PostMetaWidget/PostMetaWidget';
+export { PostSectionWidget } from './src/components/widgets/PostSectionWidget/PostSectionWidget';
